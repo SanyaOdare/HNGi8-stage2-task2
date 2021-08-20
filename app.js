7987409d47be8d030faaffde4dbe7eeec9d1ef2c
Sanya Oluwadare <sanyaodare@gmail.com>
2021-08-20 10:51:00 +0100

    show name as link

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const myBtn = document.getElementById('show-name');
 const myHeading = document.getElementById('intro');
 
 function showMyName() {
-  myHeading.innerHTML = 'My name is';
-  myBtn.innerText = 'Sanya Oluwadare';  
+  myHeading.innerHTML = 'My name is';  
+  myBtn.innerHTML = '<a href="https://hngi8-stage2-task3.netlify.app" target="_blank">Sanya Oluwadare</a>';
   console.log(`My name is ${myBtn.innerText}`);
 }
 
 myBtn.addEventListener('click', showMyName);
+
+
+/*
+const myBtn = document.getElementById('show-name');
+const myHeading = document.getElementById('intro');
+
+function showMyName() {
+  myHeading.innerHTML = 'My name is';
+  myBtn.innerText = 'Sanya Oluwadare';
+    function myLink() {
+      let a = document.createElement('a');
+      let myName = document.createTextNode('Sanya Oluwadare');
+      a.appendChild(myName);
+      a.href = 'https://hngi8-stage2-task3.netlify.app';
+}
+  // myLink();
+  console.log(`My name is ${myBtn.innerText}`);
+}
+
+myBtn.addEventListener('click', showMyName);
+*/
